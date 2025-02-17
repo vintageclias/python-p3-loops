d0cf1fd5f8334a33817754e13e6a4cc14e4d1bf5
vintageclias <vintageclias@gmail.com>
2025-02-17 11:33:13 -0800

    python loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
+    countdown = 10
+    
+    while countdown in range(10, 0, -1):
+        print(countdown)
+        countdown -= 1
+    
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [i ** 2 for i in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+       for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
+    
